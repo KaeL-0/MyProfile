acb0b71393b7bf8620b5d64f278f1ea7e3211683
Mikhael Edman Gomez <edman.gomez.business@gmail.com>
2025-02-08 17:24:50 +0800

    Added a link to script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-                            let isOriginal = true;
+                            let counter = 0;
 
                             function link() {
                                 const myLink = document.getElementById('myLink');
+
+                                counter++;
                                 
-                                if (isOriginal) {
+                                if (counter == 1) {
                                     myLink.href = "rhau_profile.html";
                                     myLink.innerHTML = "Rhaulline's Profile";
+                                } else if(counter == 2) {
+                                    myLink.href = "alt_profile.html";
+                                    myLink.innerHTML = "Alternate Profile";
                                 } else {
                                     myLink.href = "https://www.facebook.com/mikhael.edman";
                                     myLink.innerHTML = "Facebook Profile";
+                                    counter = 0;
                                 }
 
-                                isOriginal = !isOriginal;
+                                
                             }
